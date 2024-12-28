6d77f7de6df361b66c3d433449e4908ee664c465
Hubert Chruścicki <chruscicki.hubert@wp.pl>
2024-12-28 18:30:43 +0100

    select in product adding

---
+++ src/controllers/ProductsController.php
@@ -0,0 +1,141 @@
+<?php
+
+require_once 'AppController.php';
+require_once 'CategoryController.php';
+require_once dirname(__DIR__) . '/repository/CategoryRepository.php';
+require_once dirname(__DIR__) . '/repository/ProductsRepository.php';
+
+class ProductsController extends AppController
+{
+    public function addProduct()
+    {
+
+        if (!$this->isPost()) {
+            return $this->render('main');
+        }
+
+        if (session_status() !== PHP_SESSION_ACTIVE || !isset($_SESSION['id'])) {
+            $this->render('main', ['message' => 'Session problem!']);
+            return;
+        }
+        $user_id = $_SESSION['id'];
+        $id_category = (int)$_POST['productCategory']; //category by name
+        $name = $_POST['name'];
+        $price_brutto = (float)$_POST['price_brutto'];
+        $price_netto = null;
+
+
+        try {
+            $category = CategoryRepository::getInstance()->getCategoryById($id_category);
+            $vatPercent = $category->getVat();
+            $vatCalculate = 1 + $vatPercent / 100;
+            $price_netto = round($price_brutto / $vatCalculate);
+
+        } catch (Exception $e) {
+            error_log($e->getMessage());
+            throw new Exception("Failed to calculate netto price.");
+        }
+
+
+        if ($id_category === '' || $name === '' || $price_brutto === '') {
+            $this->render('main', ['message' => 'Please fill in all fields!']);
+            return;
+        }
+
+        if ((int)$price_brutto < 0) {
+            $this->render('main', ['messages' => ['Brutto price cannot be negative!']]);
+            return;
+        }
+        if ((float)$price_brutto === 0.0) {
+            $this->render('main', ['messages' => ['Brutto price cannot be 0']]);
+            return;
+        }
+
+        $productsRepository = ProductsRepository::getInstance();
+        $product = $productsRepository->getProduct((int)$user_id, $name);
+
+        if ($product) {
+            $this->render('main', ['message' => 'Such product already exists!']);
+            return;
+        }
+
+        $productsRepository->addProduct((int)$user_id, $id_category, $name, $price_brutto, $price_netto);
+        return $this->render('main', ['message' => 'Product successfully added!']);
+    }
+
+    public function howManyProducts()
+    {
+        if (!$this->isGet()) {
+            return $this->render('main');
+        }
+
+        if (session_status() !== PHP_SESSION_ACTIVE || !isset($_SESSION['id'])) {
+            $this->render('main', ['message' => 'Session problem!']);
+            return;
+        }
+        $user_id = $_SESSION['id'];
+        $namePrefix = $_GET['namePrefix'] ?? null;
+
+        $productRepositroy = ProductsRepository::getInstance();
+        $count = $productRepositroy->howManyProducts((int)$user_id, $namePrefix);
+
+        if (is_int($count)) {
+            echo json_encode(["message" => "success", "count" => $count]);
+        } else {
+            echo json_encode(["message" => "fail"]);
+        }
+
+    }
+
+    public function getProducts()
+    {
+        if (!$this->isGet()) {
+            return $this->render('main');
+        }
+
+        if (session_status() !== PHP_SESSION_ACTIVE || !isset($_SESSION['id'])) {
+            $this->render('main', ['message' => 'Session problem!']);
+        }
+
+        $user_id = $_SESSION['id'];
+
+        $limit = $_GET['limit'] ?? 10;
+        $offset = $_GET['offset'] ?? 0;
+        $namePrefix = $_GET['namePrefix'] ?? null;
+        $productRepository = ProductsRepository::getInstance();
+        $products = $productRepository->getProducts((int)$user_id, (int)$limit, (int)$offset, $namePrefix);
+
+        if (empty($products)) {
+            echo json_encode(["message" => "fail"]);
+        } else {
+            echo json_encode(["message" => "success", "products" => $products]);
+        }
+    }
+
+    public function deleteProduct()
+    {
+        if (!$this->isPost()) {
+            return $this->render('main');
+        }
+        if (session_status() !== PHP_SESSION_ACTIVE || !isset($_SESSION['id'])) {
+            $this->render('main', ['message' => 'Session problem!']);
+            return;
+        }
+        $user_id = $_SESSION['id'];
+        $productName = json_decode(file_get_contents('php://input'), true)['productName'] ?? null;
+        if($productName) {
+            $productsRepository = ProductsRepository::getInstance();
+            $deleteSuccess = $productsRepository->deleteProduct((int)$user_id, $productName);
+            if($deleteSuccess) {
+                echo json_encode(["message" => "success"]);
+            }
+            else {
+                echo json_encode(["message" => "fail", "error" => "cant delete product"]);
+            }
+        }
+        else {
+            echo json_encode(["message" => "fail", "error" => "Product name problem"]);
+        }
+    }
+
+}
---
+++ src/repository/ProductsRepository.php
@@ -0,0 +1,174 @@
+<?php
+
+require_once 'Repository.php';
+require_once dirname(__DIR__).'/models/Product.php';
+class ProductsRepository extends Repository
+{
+    protected static $instance = null;
+
+    protected function _construct()
+    {
+        parent::_construct();
+    }
+
+    public static function getInstance(): Repository
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = new ProductsRepository();
+        }
+        return self::$instance;
+    }
+
+    public function getProduct(int $user_id, string $product_name)
+    {
+        $stmt =
+            "select p.id, p.id_category, p.id_company, p.name, p.price_brutto, p.price_netto from products p
+        join company c on c.id = p.id_company
+        join users u on u.id_company = c.id
+        where is_deleted = false and u.id = :user_id
+        and LOWER(p.name) ~ :namePrefix"
+        ;
+        $prefixRegex = '^'.$product_name.'$';
+        $stmt = $this->database->connect()->prepare($stmt);
+        $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+        $stmt->bindParam(':namePrefix', $prefixRegex, PDO::PARAM_STR);
+        $stmt->execute();
+        $product = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if($product == false){
+            return null;
+        }
+
+        return new Product(
+            $product['id'],
+            $product['id_category'],
+            $product['id_company'],
+            $product['name'],
+            $product['price_brutto'],
+            $product['price_netto']
+        );
+
+    }
+
+    public function addProduct(int $user_id, int $id_category, string $name, float $price_brutto, float $price_netto) //tutaj dostac sie do company id przez usera
+    {
+        try{
+            $stmt =
+        "WITH userCompanyID as(
+                   SELECT u.id_company
+                   from users u
+                   join company c on c.id = u.id_company
+                   where u.id = ?
+                  
+            )
+            INSERT INTO products (id_category, id_company, name, price_brutto, price_netto)
+            SELECT ?, id_company, ?, ?, ?
+            FROM userCompanyID"
+            ;
+            $stmt = $this->database->connect()->prepare($stmt);
+            $stmt->execute([
+                $user_id,
+                $id_category,
+                $name,
+                $price_brutto,
+                $price_netto
+            ]);
+
+        }
+        catch(PDOException $e){
+            error_log($e->getMessage());
+            throw new Exception("Failed to add new product to database.");
+        }
+    }
+
+
+    public function howManyProducts(int $user_id, string $namePrefix=null): int
+    {
+        $baseStmt =
+            "SELECT COUNT(*)
+                    from products p
+                    join company c on c.id = p.id_company
+                    join users u on u.id_company = c.id
+                    where u.id = :user_id and is_deleted = false";
+        if($namePrefix !== null){
+            $baseStmt .= " and LOWER(p.name) ~ :namePrefix";
+        }
+        try{
+            $stmt = $this->database->connect()->prepare($baseStmt);
+            $stmt->bindParam('user_id', $user_id, PDO::PARAM_INT);
+            if($namePrefix !== null){
+                $prefixRegex = '^'.$namePrefix.'$';
+                $stmt->bindParam(':namePrefix', $prefixRegex, PDO::PARAM_STR);
+            }
+            $stmt->execute();
+            $count = $stmt->fetchColumn();
+            return (int)$count;
+        }
+        catch(PDOException $e){
+            error_log($e->getMessage());
+            return 0;
+        }
+    }
+
+
+    public function getProducts(int $user_id, int $limit, int $offset, string $namePrefix=null)
+    {
+        try {
+            $baseStmt =
+                "select p.id, p.id_category, p.id_company, p.name, pc.name as category_name, pc.vat, ROUND((pc.vat/100 * p.price_brutto),2)  as vat_value, p.price_brutto, p.price_netto, p.is_deleted from products p
+            join company c on c.id = p.id_company
+            join users u on u.id_company = c.id
+            join product_categories pc on pc.id = p.id_category                                                                                  
+            where p.is_deleted = false and u.id = :user_id"
+            ;
+            if($namePrefix !== null) {
+                $baseStmt .= " AND LOWER(p.name) ~ :namePrefix ";
+            }
+            $baseStmt .= " ORDER BY p.name
+                           LIMIT :limit 
+                           offset :offset";
+            $stmt = $this->database->connect()->prepare($baseStmt);
+            if ($namePrefix !== null) {
+                $prefixRegex = '^' . $namePrefix;
+                $stmt->bindParam(":namePrefix", $prefixRegex, PDO::PARAM_STR);
+            }
+            $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+            $stmt->bindParam(':limit', $limit, PDO::PARAM_INT);
+            $stmt->bindParam(':offset', $offset, PDO::PARAM_INT);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+        catch (PDOException $e){
+            error_log("Database error: " . $e->getMessage());
+            return 0;
+        }
+    }
+
+    public function deleteProduct(int $user_id, string $product_name): bool
+    {
+        $baseStmt = "
+            UPDATE public.products p
+            SET is_deleted = true
+            FROM company c
+            JOIN users u ON u.id_company = c.id
+            WHERE
+            c.id = p.id_company
+            AND LOWER(p.name) = :product_name
+            AND u.id = :user_id
+            AND p.is_deleted = false;";
+
+        $stmt = $this->database->connect()->prepare($baseStmt);
+
+        $stmt->bindParam(":product_name", $product_name, PDO::PARAM_STR);
+        $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+        try {
+            $stmt->execute();
+            return true;
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+            return false;
+        }
+    }
+
+
+}
